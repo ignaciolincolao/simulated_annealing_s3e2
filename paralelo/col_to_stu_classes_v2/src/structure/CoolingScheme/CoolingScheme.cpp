853e41024bc8eab71acab3c0abc763c6fd177739
0mellado <omellao@protonmail.com>
2023-10-26 13:49:09 -0300

    on ready base branch

--- paralelo/col_to_stu_classes_v2/src/structure/CoolingScheme/CoolingScheme.cpp
+++
@@ -1,6 +0,0 @@
-#include <structure/CoolingScheme/CoolingScheme.hpp>
-
-void CS2::apply()
-{
-    saParams.temp = (saParams.temp * (csParams.coolingRate));
-}
--- paralelo/optBayesian/blockCol_threadEst/src/CoolingScheme.cpp
+++
@@ -1,8 +0,0 @@
-#include <CoolingScheme.hpp>
-
-
-
-void coolingCS2(double &temp,double coolingRate)
-{
-    temp=temp*(coolingRate);
-}
